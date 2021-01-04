64ce74c3e5dde64dfcb8df15594cb53d9c235e2d
Jeff Bai <j.bai@wustl.edu>
2021-01-04 05:56:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(customers)
     puts "The line is currently empty."
   else
     line = "The line is currently: "
-    customers.each_with_index do |index+1, customer|
-      line << "#{index}. #{customer} "
+    customers.each_with_index do |customer, index|
+      line << "#{index+1}. #{customer} "
     end
     puts line
   end
